968b45bf5c8e28aba011e9f87be158dbaca967d9
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2023-06-09 12:37:31 +0530

    Update

--- face-app2.py
+++ face-app2.py
@@ -3,25 +3,21 @@ import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
 import numpy as np
-import joblib
 from datetime import datetime, date
 from tkinter import ttk
 import connect_mongo as db
 import pyttsx3
-import mediapipe as mp
-import tensorflow.keras.models as models
 from keras.models import load_model
 
 def capture():
     def take_attendance(pred):
         print(pred)
-        # current_time = datetime.now().time().strftime('%H:%M:%S')
-        # current_date = date.today().strftime('%d-%m-%Y')
-        # db.insert_data(emp_dict[pred],pred, current_date, current_time)
-        # speak(f"Hi {pred} your attendace is saved")
-        
+        current_time = datetime.now().time().strftime('%H:%M:%S')
+        current_date = date.today().strftime('%d-%m-%Y')
+        db.insert_data(emp_dict[pred],pred, current_date, current_time)
+        speak(f"Hi {pred} your attendace is saved")
     def check_key(event):
-        if event.keysym == 's':
+        if event.keysym == 's': 
             take_attendance(labels[predicted_class])
             
     root.bind('<Key>', check_key)
@@ -44,13 +40,15 @@ def capture():
             predicted_class = np.argmax(predictions)
             confidence = predictions[0][predicted_class] * 100
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.putText(frame, labels[predicted_class], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-            cv2.putText(frame, 'Confidence: {:.2f}%'.format(confidence), (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+            if confidence > 70: 
+                cv2.putText(frame, labels[predicted_class], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                cv2.putText(frame, 'Confidence: {:.2f}%'.format(confidence), (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
         img2 = Image.fromarray(frame)
         imgtk = ImageTk.PhotoImage(image = img2)
         L1['image'] = imgtk
         root.update()
-             
+
+
 
 def speak(text):
     engine = pyttsx3.init()
@@ -60,16 +58,9 @@ def speak(text):
 
 # Load the haarcascade for face detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-# Load the BlazeFace model
-mp_face_detection = mp.solutions.face_detection
-face_detection = mp_face_detection.FaceDetection()
+
 # Load the saved model
-model = load_model('unk_vgg_model.h5')
-
-# pca = joblib.load('unk_pca.pkl')
-# # Load your trained SVM classifier
-# svm = joblib.load('unk_knn.pkl')
-
+model = load_model('unk_vgg_model3.h5')
 # Access the webcam
 cap = cv2.VideoCapture(0)
 kernel = np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]])
